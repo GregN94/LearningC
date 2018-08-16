c4cf33d6f7637fb5ab27ee60e5c7f91b8cbc88b9
Grzegorz Nowinski <grzesiekn1001@gmail.com>
2018-08-16 14:27:59 +0200

    Added writing and reading from bin

--- 11_01.c
+++ 11_01.c
@@ -15,6 +15,7 @@ typedef struct
     char name[30];
     char surname[30];
     unsigned int age;
+    Date date_birth;
     Date date_joined;
     char gender;
 } Student;
@@ -51,7 +52,7 @@ void getThreeValues(char* textToPrint, char* format, void* mem,
                                                      void* mem2,
                                                      void* mem3);
 void copyStudents(Student* destination, Student* source);
-void swapStudents(int index);
+void swapStudents(Student* left, Student* right);
 void printStudentByIndex(int index);
 void printAllStudents();
 void printOneStudent();
@@ -62,11 +63,12 @@ void readStudentsFromFile();
 void writeStudentsToFile();
 void printSizeOfFile(char* fileName);
 int getNumOfLines(char* fileName);
-void txtWrite(Student* students, char* fileName, int numOfStudents);
+void txtWrite(char* fileName);
+void binWrite(char* fileName);
 int getAge(Date* birthDate);
 void printMenu();
 void sortStudents();
-void sort(bool checkFunc(int index));
+void sort(bool checkFunc(Student left, Student right));
 
 int main()
 {
@@ -224,21 +226,57 @@ void writeStudentsToFile()
     char * fileName = (char*) malloc(sizeof(char) * 30);
     getOneValue("\nPut file name to write to: ", " %s", fileName);
 
-    txtWrite(students, fileName, numOfStudents);
+    char option = '1';
+    printf("\nT write to txt format put 1, to write to binary write 2");
+    getOneValue("\nPut option: ", " %c", &option);
+    switch(option)
+    {
+        case '1':
+            txtWrite(fileName);
+            printSizeOfFile(fileName);
+            break;
+        case '2':
+            binWrite(fileName);
+            printSizeOfFile(fileName);
+            break;
+        default:
+            printf("\nSelected wrong option!!!\n");
+    }
 
-    printSizeOfFile(fileName);
     free(fileName);
 }
 
-void txtWrite(Student* students, char* fileName, int numOfStudents)
+void binWrite(char* fileName)
+{
+    FILE* file = fopen(fileName, "w+b");
+    for (int i = 0; i < numOfStudents; ++i)
+    {
+        fwrite(students[i].name, sizeof(char), strlen(students[i].name), file);
+        fwrite(" ", sizeof(char), 1, file);
+        fwrite(students[i].surname, sizeof(char), strlen(students[i].surname), file);
+        fwrite(" ", sizeof(char), 1, file);
+        fwrite(&students[i].date_birth.year, sizeof(int), 1, file);
+        fwrite(&students[i].date_birth.month, sizeof(int), 1, file);
+        fwrite(&students[i].date_birth.day, sizeof(int), 1, file);
+        fwrite(&students[i].date_joined.year, sizeof(int), 1, file);
+        fwrite(&students[i].date_joined.month, sizeof(int), 1, file);
+        fwrite(&students[i].date_joined.day, sizeof(int), 1, file);
+        fwrite(&students[i].gender, sizeof(char), 1, file);
+    }
+    fclose(file);
+}
+
+void txtWrite(char* fileName)
 {
     FILE* file = fopen(fileName, "w+t");
     for (int i = 0; i < numOfStudents; ++i)
     {
-        fprintf(file, "%s %s %d %d-%d-%d %c\n",
+        fprintf(file, "%s %s %d-%d-%d %d-%d-%d %c\n",
                 students[i].name,
                 students[i].surname,
-                students[i].age,
+                students[i].date_birth.day,
+                students[i].date_birth.month,
+                students[i].date_birth.year,
                 students[i].date_joined.day,
                 students[i].date_joined.month,
                 students[i].date_joined.year,
@@ -266,13 +304,12 @@ int getNumOfLines(char* fileName)
 
 void getStudentFromLine(char* line)
 {
-    Date birthDate;
     sscanf(line, "%s %s %d-%d-%d, %d-%d-%d, %c",
                 students[numOfStudents].name,
                 students[numOfStudents].surname,
-                &birthDate.year,
-                &birthDate.month,
-                &birthDate.day,
+                &students[numOfStudents].date_birth.year,
+                &students[numOfStudents].date_birth.month,
+                &students[numOfStudents].date_birth.day,
                 &students[numOfStudents].date_joined.year,
                 &students[numOfStudents].date_joined.month,
                 &students[numOfStudents].date_joined.day,
@@ -281,24 +318,70 @@ void getStudentFromLine(char* line)
     size_t length = strlen(students[numOfStudents].surname) - 1;
     char * end = students[numOfStudents].surname + length;
     *end = '\0';
-    students[numOfStudents].age = getAge(&birthDate);
+    students[numOfStudents].age = getAge(&students[numOfStudents].date_birth);
 }
 
-void readStudentsFromFile()
+char* readStringFromBinary(FILE* file)
 {
-    char * fileName = (char*)malloc(sizeof(char) * 30);
-    getOneValue("\nPut file name to read from: ", " %s", fileName);
+    char* string = malloc(sizeof(char) * 20);
+    int length = 0;
+    unsigned char buf;
+    while(fread(&buf, sizeof(buf), 1, file))
+    {
+        if(buf == ' ') break;
+        string[length] = buf;
+        length++;
+    }
+    string[length] = '\0';
+    if (length == 0) return "";
+    return string;
+}
+
+Student readStudentFromBin(FILE* file)
+{
+    Student temp;
+    char* name = readStringFromBinary(file);
+    strcpy(temp.name, name);
+    strcpy(temp.surname, readStringFromBinary(file));
+    fread(&temp.date_birth.year, sizeof(int), 1, file);
+    fread(&temp.date_birth.month, sizeof(int), 1, file);
+    fread(&temp.date_birth.day, sizeof(int), 1, file);
+    fread(&temp.date_joined.year, sizeof(int), 1, file);
+    fread(&temp.date_joined.month, sizeof(int), 1, file);
+    fread(&temp.date_joined.day, sizeof(int), 1, file);
+    fread(&temp.gender, sizeof(char), 1, file);
+    return temp;
+}
+
+void readFromBin(char* fileName)
+{
+    FILE* file = fopen(fileName, "rb");
+
+    if (file != NULL)
+    {
+        while (true)
+        {
+            Student tempStudent = readStudentFromBin(file);
+            if (strlen(tempStudent.name) == 0) break;
+            numOfStudents++;
+            students = (Student*)realloc(students,sizeof(Student) * numOfStudents );
+            copyStudents(&students[numOfStudents -1], &tempStudent);
+        }
+    }
+}
+
+void readFromTxt(char* fileName)
+{
     FILE* file = fopen(fileName, "rt");
 
-    if (strlen(fileName) == 0) printf("hello");
     if (file != NULL)
     {
         printSizeOfFile(fileName);
 
-        char line[110] = {""};
-
         int allocationNumber = numOfStudents + getNumOfLines(fileName);
         students = (Student*)realloc(students,sizeof(Student) * allocationNumber );
+
+        char line[110] = {""};
         while (fgets(line, sizeof(line), file) != NULL)
         {
             getStudentFromLine(line);
@@ -309,6 +392,21 @@ void readStudentsFromFile()
     else
     {
         printf("ERROR: Couldn't open file!!!\n");
+    }
+}
+
+void readStudentsFromFile()
+{
+    char * fileName = (char*)malloc(sizeof(char) * 30);
+    getOneValue("\nPut file name to read from: ", " %s", fileName);
+    char option = '1';
+    printf("\nRead from txt option 1, form binary 2");
+    getOneValue("\nPut option to read from: ", " %c", &option);
+
+    switch(option)
+    {
+        case '1'     :    readFromTxt(fileName);break;
+        case '2'     :    readFromBin(fileName);break;
     }
     free(fileName);
 }
@@ -337,37 +435,37 @@ void printSortingOptions()
     printf("\n4. Date joined");
 }
 
-bool byName(int index)
+bool byName(Student left, Student right)
 {
-    return *students[index].name > *students[index + 1].name;
+    return *left.name > *right.name;
 }
 
-bool bySurname(int index)
+bool bySurname(Student left, Student right)
 {
-    return *students[index].surname > *students[index + 1].surname;
+    return *left.surname > *right.surname;
 }
 
-bool byAge(int index)
+bool byAge(Student left, Student right)
 {
-    return students[index].age > students[index + 1].age;
+    return left.age > right.age;
 }
 
-bool byJoinedDate(int index)
+bool byJoinedDate(Student left, Student right)
 {
     bool isBigger = false;
-    if ( students[index].date_joined.year > students[index + 1].date_joined.year)
+    if ( left.date_joined.year > right.date_joined.year)
     {
         isBigger = true;
     }
-    else if ( students[index].date_joined.year == students[index + 1].date_joined.year)
+    else if ( left.date_joined.year == right.date_joined.year)
     {
-        if ( students[index].date_joined.month > students[index + 1].date_joined.month)
+        if ( left.date_joined.month > right.date_joined.month)
         {
             isBigger = true;
         }
-        else if ( students[index].date_joined.month == students[index + 1].date_joined.month)
+        else if ( left.date_joined.month == right.date_joined.month)
         {
-            if ( students[index].date_joined.day > students[index + 1].date_joined.day)
+            if ( left.date_joined.day > right.date_joined.day)
             {
                 isBigger = true;
             }
@@ -376,24 +474,24 @@ bool byJoinedDate(int index)
     return isBigger;
 }
 
-void sort(bool checkFunc(int index))
+void sort(bool checkFunc(Student left, Student right))
 {
     for (int i = 1; i <= numOfStudents; ++i)
     {
         for (int j = 0; j < numOfStudents - i; ++j)
         {
-            if (checkFunc(j))
-                swapStudents(j);
+            if (checkFunc(students[j], students[j + 1]))
+                swapStudents(&students[j], &students[j + 1]);
         }
     }
 }
 
-void swapStudents(int index)
+void swapStudents(Student* left, Student* right)
 {
     Student tempStudent;
-    copyStudents(&tempStudent, &students[index]);
-    copyStudents(&students[index], &students[index + 1]);
-    copyStudents(&students[index + 1], &tempStudent);
+    copyStudents(&tempStudent, left);
+    copyStudents(left, right);
+    copyStudents(right, &tempStudent);
 }
 
 void sortStudents()
