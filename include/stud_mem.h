80b7f936f4180598b7c0ddaf14182f390f94e06e
GregN94 <grzesiekn1001@gmail.com>
2018-10-21 23:45:21 +0200

    Own implementation of memcpy, memset and memcmp

--- include/stud_mem.h
+++ include/stud_mem.h
@@ -1,4 +1,10 @@
 #ifndef RUN_STUD_MEM_H
 #define RUN_STUD_MEM_H
 
+#include <stddef.h>
+
+void* my_memcpy(void *dest, const void *src, size_t count );
+int my_memcmp ( const void * ptr1, const void * ptr2, size_t num );
+void* my_memset( void* dest, int ch, size_t count );
+
 #endif //RUN_STUD_MEM_H
--- src/stud_mem.c
+++ src/stud_mem.c
@@ -1,3 +1,39 @@
 #include "stud_mem.h"
 
-void get(){}
+void* my_memcpy( void *dest, const void *src, size_t count )
+{
+    unsigned char* dest_ptr = (unsigned char*) dest;
+    unsigned char* src_ptr = (unsigned char*) src;
+    for (size_t i = 0; i < count; ++i)
+    {
+        *(dest_ptr++) = *(src_ptr++);
+    }
+    return dest;
+}
+
+int my_memcmp ( const void * ptr1, const void * ptr2, size_t num )
+{
+    unsigned char* ptr1_chr = (unsigned char*) ptr1;
+    unsigned char* ptr2_chr = (unsigned char*) ptr2;
+
+    for (size_t i = 0; i < num; ++i)
+    {
+        if ( *ptr1_chr < * ptr2_chr ) return -1;
+        else if ( *ptr1_chr > * ptr2_chr ) return 1;
+        ptr1_chr++;
+        ptr2_chr++;
+    }
+    return 0;
+}
+
+void* my_memset( void* dest, int ch, size_t count )
+{
+    unsigned char* dest_chr = (unsigned char*) dest;
+
+    for (size_t i = 0; i < count; ++i)
+    {
+        *dest_chr = (unsigned char) ch;
+        dest_chr++;
+    }
+    return dest;
+}
